0e5cd61a25a302896522fad584401ae2d50d0dbf
milyasbpa <basilyas.2015@gmail.com>
2020-07-14 12:01:44 +0700

    Updates

---
+++ precache-manifest.e116339f2582a207d6b4f755b2f42600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b1ec788bbde4197c4f72fef244e9e2",
+    "url": "/penilaianbpjs/index.html"
+  },
+  {
+    "revision": "68b645ff116f8396aea0",
+    "url": "/penilaianbpjs/static/css/main.474bbf9e.chunk.css"
+  },
+  {
+    "revision": "db496b01509806d713f7",
+    "url": "/penilaianbpjs/static/js/2.e4786279.chunk.js"
+  },
+  {
+    "revision": "32aeafb3e2d0a73f27822c8ed8045b88",
+    "url": "/penilaianbpjs/static/js/2.e4786279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b645ff116f8396aea0",
+    "url": "/penilaianbpjs/static/js/main.7b4e207c.chunk.js"
+  },
+  {
+    "revision": "39c7706e06c89e4d7749",
+    "url": "/penilaianbpjs/static/js/runtime-main.bdbb745e.js"
+  }
+]);
